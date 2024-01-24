2684302b92f4f7c0efe638b4f606c4a6e20f2262
dev1webdogu <156883060+dev1webdogu@users.noreply.github.com>
2024-01-24 13:58:35 +0900

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,6 @@ import store from './vuex/store'
 
 const app = createApp(App)
 app.config.globalProperties.$axios = axios;  //전역변수로 설정 컴포넌트에서 this.$axios 호출할 수 있음
-app.config.globalProperties.$serverUrl = '//localhost:8081' //api server
+app.config.globalProperties.$serverUrl = '//https://port-0-backjava-azyqdr152alrq4oq4w.sel5.cloudtype.app/' //api server
 app.config.globalProperties.$store = store
 app.use(router).use(store).mount('#app')
